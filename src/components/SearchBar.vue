8a2d6cf20bfee142ab80edb83bf979cc95e3df21
aerocat <mferri1@depaul.edu>
2019-03-25 18:38:25 -0500

    Auto-complete search input when only a matching result is found :)

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -10,10 +10,6 @@
                    @keydown.esc="onEsc"
                    placeholder="Enter a stock name or ticker"/>
             <input type="submit" value="Search" class="btn"/>
-
-            <!-- <datalist id="stocks">
-                <option v-for="ticker in tickersAndCompanies" v-bind:key="ticker"> {{ticker}} </option>
-            </datalist> -->
         </form>
 
         <div class="search-dropdown">
@@ -23,6 +19,7 @@
                         @click="setResult(result)"
                         class="search-result"
                         v-bind:class="{ 'is-active': index === arrowCounter }"> {{ result }} </li>
+                    <li v-if="filteredResults.length == 0"> No Stocks Found :( </li>
             </ul>
         </div>
         <!-- <h3> userInput is: {{ userInput }} </h3> -->
@@ -43,30 +40,20 @@ export default {
             arrowCounter: -1
         }
     },
-    created: function () {
-        // let lst = [
-        //             "AAPL - Apple Inc." ,
-        //             "FB - Facebook Inc." ,
-        //             "AMZN - Amazon Inc." ,
-        //             "MSFT - Microsoft Inc." ]
-        
+    created: function () {        
         let url = `http://localhost:3000/api/companies`;
         axios.get(url)
             .then(res => {
                 this.tickersAndCompanies = res.data;
             })
             .catch(err => console.log(err));
-
-        // I could use a foreach or map to filter/format data how I want it
-        // ...
-
-        // this.tickersAndCompanies = lst;
     },
     methods: {
         searchStock(e) {
             e.preventDefault(); // preventing default form behavior (sending data to a file)
             let ticker = this.userInput.split('-')[1].trim();
             let companyName = this.userInput.split('-')[0].trim();
+            
             console.log('about to search:', ticker);
             // TODO: Add validation so user can't search something invalid
             // like if ticker not in tickers, disable search button
@@ -83,12 +70,20 @@ export default {
             this.userInput = '';
         },
         onChange() {
+            // If user hasn't entered text or has deleted it
             if (this.userInput === '') {
                 this.isOpen = false;
                 this.arrowCounter = -1;
-            } else {
+            }
+            // else, input is valid
+            else {
                 this.isOpen = true;
                 this.filterResults();
+
+                // Autocomplete field if there's only one result found
+                if (this.filteredResults.length == 1) {
+                    this.setResult(this.filteredResults[0]);
+                }
             }
         },
         filterResults() {
@@ -96,10 +91,7 @@ export default {
                 // yields only companies where userInput appears in their string (a.k.a indexOf returns a non-negative index)
                 company.toLowerCase().indexOf(this.userInput.toLowerCase()) > -1
             );
-            // if (this.filteredResults.length != 0) {
-            //     this.arrowCounter = 0;
-            //     console.log('set this.arrowcounter to: ', this.arrowCounter);
-            // }
+            console.log('Matching results:', this.filteredResults.length);
         },
         setResult(selectedResult) {
             this.arrowCounter = -1;
